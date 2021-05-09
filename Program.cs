58136ea162f04799d45fe4bd20ba7f0c706c9c47
Patil-94 <ankitakhairnar10@.com>
2021-05-09 16:17:58 +0530

    [Ankita] Add:Find Frequency of Words

--- Program.cs
+++ Program.cs
@@ -6,44 +6,87 @@ using System.Threading.Tasks;
 
 namespace HashTable
 {
-    class Program
+   public  class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("-----Welcome To HashTable Program-----");
 
-            MyMapNode<int, string> hash = new MyMapNode<int, string>(15);//creating object and Key and value datatype is string and size is 5
-           
+            MyMapNode<string, int> hash = new MyMapNode<string, int>(5);
 
-            //creating a string of sentence
-            string phrase = "Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations";
-
+            //creating a string of long sentence
+            string words = "Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations";
             //adding values into array
-            string[] words = phrase.Split(' ');//split method is used 
-                                               //split string into substrings
+            string[] arr = words.Split(' ');
 
-            int a = 1;
-            foreach (var element in words)
+            //creating a linkedlist to check for duplicates value
+            LinkedList<string> checkForDuplication = new LinkedList<string>();
+
+            //iterating loop over the array of sentence words
+            foreach (string element in arr)
             {
-                hash.Add(a, element );
-                a++;
+                 int count = 0;
+                //another foreach loop to check if other same element is present
+                //if present counter is incremented
+                foreach (string match in arr)
+                {
+                    if (element == match)
+                    {
+                        count++;
+                        //if same element is coming in 2nd time, it will be presnent in linkedlist, so loopwill break and no further counting will be done
+                        if (checkForDuplication.Contains(element))
+                        {
+                            break;
+                        }
+                    }
 
+                }
+                //if element is already there in list, outer loop will be continued and loop will shift to next value
+                //basically only values appearing for 1st time, will be added in linkedlist and added in hash table, so that frequency can be displayed.
+                if (checkForDuplication.Contains(element))
+                {
+                    continue;
+                }
+                //added element in linkedlist
+                checkForDuplication.AddLast(element);
+                //added element and it's frequency in hashtable.
+                hash.Add(element, count);
             }
+            //getting the specific value from hashtable.
+            int frequency = hash.Get("they");
+            Console.WriteLine("frequency for they:\t" + frequency);
 
-            int index = 18;
-            string choice = hash.Get(index);  // getting the specific value from hashtable.
-            Console.WriteLine("{0}th index values : is {1}", index, choice);
-
-            int index2 = 15;
-            string choice2 = hash.Get(index2);
-            Console.WriteLine("{0}th index values : is {1}", index2, choice2);
-           
-            hash.Remove(index);//remove 18 index
+            //Displaying all the elements from the linkedlist
+            Console.WriteLine();
+            Console.WriteLine("Displaying all the key value pairs in hash table");
             hash.Display();
+
+          
             Console.Read();
 
 
         }
     }
-          
 }
+//{
+//    hash.Add(a, element );
+//    a++;
+
+//}
+
+//int index = 18;
+//string choice = hash.Get(index);  // getting the specific value from hashtable.
+//Console.WriteLine("{0}th index values : is {1}", index, choice);
+
+//int index2 = 15;
+//string choice2 = hash.Get(index2);
+//Console.WriteLine("{0}th index values : is {1}", index2, choice2);
+
+//hash.Remove(index);//remove 18 index
+//hash.Display();
+//Console.Read();
+
+
+
+
+
